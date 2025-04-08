c650c1697fbfc043d5f06bd89e4eae923ac5fe1e
johnny-kiv <johnny.kiv@yandex.ru>
2025-04-08 12:47:06 +0300

    pinpong ver1

--- pinpong.py
+++ pinpong.py
@@ -3,9 +3,9 @@ from random import Random
 import pygame
 import random
 class enemy:
-    def __init__(self,vector,x,color):
+    def __init__(self,vector,x,width,color):
         self.vector = vector
-        self.width = 60
+        self.width = width
         self.height = 10
         self.color = color
         self.x = x
@@ -39,28 +39,33 @@ class ball:
         self.y = 50
         self.d = 10
         self.w = -1
-        self.spx = 2
-        self.spy = 2
+        self.spx = 5
+        self.spy = 5
     def walk(self):
         pygame.draw.circle(screen,self.color,(self.x,self.y),self.d)
         self.x+=self.spx
         self.y+=self.spy
-    def usl(self,hx,enx):
+    def usl(self,hx,hw,enx):
         if self.x+self.d>=400 or self.x<=0:
             self.spx = -self.spx
-        if self.y ==340 and (self.x>=hx and self.x<=hx+80):
+        if (self.y ==350 or self.y==360) and (self.x>=hx and self.x<=hx+hw):
+            self.spy = -self.spy
+        if (self.y ==350 or self.y==360) and (self.x>=hx and self.x<=hx+hw):
             self.spy = -self.spy
         for i in enx:
-            if self.y == 60 and (self.x >= i and self.x <= i + 60):
+            if (self.y ==50 or self.y==60) and (self.x >= i[0] and self.x <= i[0] + i[1]):
                 self.spy = -self.spy
+            if (self.y >=50 or self.y<=60) and (self.x == i[0] and self.x == i[0] + i[1]):
+                self.spx = -self.spx
         if self.y<=0 or self.y>=400:
             self.spy = -self.spy
 pygame.init()
 screen = pygame.display.set_mode((400,400))
 clock = pygame.time.Clock()
 done = False
-en1 = enemy(-10,335,"red")
-en2 = enemy(10,5,"orange")
+en1 = enemy(-10,335,60,"red")
+en2 = enemy(10,5, 80,"orange")
+en3 = enemy(5,200,60,"yellow")
 h = hero()
 b = ball()
 while not done:
@@ -69,9 +74,11 @@ while not done:
             done = True
     h.walk()
     b.walk()
-    b.usl(h.x,[en1.x,en2.x])
+    b.usl(h.x,h.width,[[en1.x,en1.width],[en2.x,en2.width]])
     en1.walk()
     en1.usl()
+    en3.walk()
+    en3.usl()
     en2.walk()
     en2.usl()
     pygame.display.flip()
